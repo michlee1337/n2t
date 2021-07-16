154ddf5c4a1f56701a057562cd6d736a69ad8ea1
Michelle <michlee1337@gmail.com>
2021-07-16 19:36:22 -0400

    constructor and getnext

--- 06/assembler/parser.cc
+++ 06/assembler/parser.cc
@@ -1,22 +1,54 @@
-#include "assembler.h"
+#include "parser.h"
 
 namespace n2t {
 
-Parser::Parser(std::istream& is) {
-    // TODO: PARSE SYMBOLS
-    current_line_number_(0);
-    f_(is);
+Parser::Parser(std::string asm_file) {
+    asm_file_.open(asm_file);
+    cur_line_number_ = -1;
 }
 
 bool Parser::get_next() {
-    while (std.getline(f_, current_line_) {
-        ++current_line_number_;
-        if (!line.empty() && !isComment(line)) {
-            return True;
+    while (std::getline(asm_file_, cur_line_)) {
+        ++cur_line_number_;
+        if (!cur_line_.empty() && !isComment()) {
+            return true;
         }
     }
-    return False
+    return false;
 }
 
+    CommandType Parser::commandType() {
+        if (cur_line_[0] == '@') {
+            return A_COMMAND;
+        }
+        return C_COMMAND;
+    }
 
+    std::string Parser::addr() {
+        return "";
+    };
+
+    std::string Parser::dest() {
+        return "";
+    };
+
+    std::string Parser::comp() {
+        return "";
+    };
+
+    std::string Parser::jump() {
+        return "";
+    };
+
+int Parser::cur_line_number() {
+    return cur_line_number_;
+}
+
+std::string Parser::cur_line() {
+    return cur_line_;
+}
+
+bool Parser::isComment() {
+    return (cur_line_[0] == '/' && cur_line_[1] == '/');
+}
 }
