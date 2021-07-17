07e1a4b16f9243692d544f094ed445ec4aa27e4e
Michelle <michlee1337@gmail.com>
2021-07-17 14:42:22 -0400

    add semicolons

--- 06/assembler/parser.cc
+++ 06/assembler/parser.cc
@@ -5,7 +5,7 @@ namespace n2t {
 Parser::Parser(std::string asm_file) {
     asm_file_.open(asm_file);
     cur_line_number_ = 0;
-}
+};
 
 bool Parser::get_next() {
     while (std::getline(asm_file_, cur_line_)) {
@@ -15,14 +15,14 @@ bool Parser::get_next() {
         }
     }
     return false;
-}
+};
 
 CommandType Parser::commandType() {
     if (cur_line_[0] == '@') {
         return A_COMMAND;
     }
     return C_COMMAND;
-}
+};
 
 std::string Parser::addr() {
     if (commandType() != A_COMMAND) {
@@ -58,18 +58,18 @@ std::string Parser::jump() {
 
 int Parser::cur_line_number() {
     return cur_line_number_;
-}
+};
 
 std::string Parser::cur_line() {
     return cur_line_;
-}
+};
 
 bool Parser::isComment() {
     return (cur_line_[0] == '/' && cur_line_[1] == '/');
-}
+};
 
 bool Parser::isBlank() {
-    return (std::all_of(cur_line_.begin(),cur_line_.end(),isspace));
-}
+    return std::all_of(cur_line_.begin(),cur_line_.end(),isspace);
+};
 
 }
