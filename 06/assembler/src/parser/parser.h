25d3d2af806a254d8b0e994383c65a501b241b31
Michelle <michlee1337@gmail.com>
2021-07-26 18:41:26 -0400

    style

--- 06/assembler/src/parser/parser.h
+++ 06/assembler/src/parser/parser.h
@@ -1,5 +1,5 @@
-#ifndef NAND2TETRIS_PARSER
-#define NAND2TETRIS_PARSER
+#ifndef PROJECTS_06_ASSEMBLER_SRC_PARSER_PARSER_H_
+#define PROJECTS_06_ASSEMBLER_SRC_PARSER_PARSER_H_
 
 #include <string>
 #include <unordered_map>
@@ -12,31 +12,30 @@ namespace n2t {
 enum CommandType {A_COMMAND, C_COMMAND};
 
 class Parser {
+ private:
+  std::ifstream asm_file_;
+  SymbolTable symbol_table_;
+  std::string cur_line_;
+  bool isSymbol();
+  bool isLabel();
+  bool isComment();
+  bool isBlank();
+  void trim();
 
-private:
-	std::ifstream 	asm_file_;
-    SymbolTable symbol_table_;
-	std::string cur_line_;
-    bool isSymbol();
-    bool isLabel();
-    bool isComment();
-    bool isBlank();
-    void trim();
+ public:
+  explicit Parser(std::string asm_file);  // parse symbols on construction
 
-public:
-	Parser(std::string asm_file);  // parse symbols on construction
+  // parsed elements of current command
+  std::string addr();
+  std::string dest();
+  std::string comp();
+  std::string jump();
 
-    // parsed elements of current command
-    std::string addr();
-    std::string dest();
-    std::string comp();
-    std::string jump();
-
-    bool get_next();  // returns False if end of file
-    CommandType commandType();
-    std::string cur_line();
+  bool get_next();  // returns False if end of file
+  CommandType commandType();
+  std::string cur_line();
 };
 
-}
+}  // namespace n2t
 
-#endif
+#endif  // PROJECTS_06_ASSEMBLER_SRC_PARSER_PARSER_H_
