8e7e6a3bda2a4e303ff7a4ea1645eccdedb175e6
Michelle <michlee1337@gmail.com>
2021-07-26 18:52:53 -0400

    styel

--- 06/assembler/src/translator/translator.cc
+++ 06/assembler/src/translator/translator.cc
@@ -3,20 +3,20 @@
 namespace n2t {
 
 std::bitset<16> Translator::addr(std::string addr_asm) {
-    std::bitset<16> addr(std::stoi(addr_asm));
-    return addr;
+  std::bitset<16> addr(std::stoi(addr_asm));
+  return addr;
 }
 
 std::bitset<16> Translator::dest(std::string dest_asm) {
-    return dest_map_[dest_asm];
+  return dest_map_[dest_asm];
 }
 
 std::bitset<16> Translator::comp(std::string comp_asm) {
-    return comp_map_[comp_asm];
+  return comp_map_[comp_asm];
 }
 
 std::bitset<16> Translator::jump(std::string jump_asm) {
-    return jump_map_[jump_asm];
+  return jump_map_[jump_asm];
 }
 
-}
+}  // namespace n2t
