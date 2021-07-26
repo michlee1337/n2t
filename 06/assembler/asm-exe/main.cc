df49ea955c4031efc492e09cff200414cb611513
Michelle <michlee1337@gmail.com>
2021-07-26 18:35:10 -0400

    style

--- 06/assembler/asm-exe/main.cc
+++ 06/assembler/asm-exe/main.cc
@@ -1,16 +1,15 @@
 #include <iostream>
 #include "assembler/assembler.h"
 
-int main(int argc, char* argv[])
-{
-   if (argc != 2) {
-      std::cout << "only excepts arguments format [asm.cc <Prog.asm>]" << std::endl;
-      return -1;
-   }
-   // assembler.process(std::string(argv[1]));
-   n2t::Assembler assembler;
-   assembler.assemble(std::string(argv[1]));
+const char errArgFormat[] = "Only excepts arguments format [asm.cc <Prog.asm>]";
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << errArgFormat << std::endl;
+    return -1;
+  }
+  // assembler.process(std::string(argv[1]));
+  n2t::Assembler assembler;
+  assembler.assemble(std::string(argv[1]));
 
-   return 0;
-
+  return 0;
 }
