1c68588e19660b961db883a4686008be160d9125
Michelle <michlee1337@gmail.com>
2021-08-09 15:58:08 -0400

    advance

--- 07/vm_translator/src/parser/parser.cc
+++ 07/vm_translator/src/parser/parser.cc
@@ -14,4 +14,16 @@ bool Parser::hasMoreCommands() {
   return false;
 }
 
+void Parser::advance() {
+  do {
+    getline(in_, cur_line_);
+  }
+  while (!isCommand());
+  
+  // rmv comments
+  size_t i = cur_line_.find_first_of('/');
+  if (i != std::string::npos) {
+    cur_line_.erase(i);
+  }
+
 }  // namespace n2t
