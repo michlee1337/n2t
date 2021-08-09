a19181bb68f3565405a96e9bac684befc3b5b80d
Michelle <michlee1337@gmail.com>
2021-08-09 15:57:09 -0400

    blank impl

---
+++ 07/vm_translator/src/coder/coder.cc
@@ -0,0 +1,17 @@
+#include "coder/coder.h"
+
+namespace n2t {
+
+int Coder::writeArithmetic() {
+  return 0;
+}
+
+int Coder::writePushPop() {
+  return 0;
+}
+
+int Coder::close() {
+  return 0;
+}
+
+}  // namespace n2t
